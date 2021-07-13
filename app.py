48345df64e3d178742b31d7d5b3cd5e509172cd1
Abhishek Kumar <abhishekk@zendrive.com>
2021-07-13 22:09:16 +0530

    app changes

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+@app.route('/hello/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return 'Hello World!\n'
+
+@app.route('/hello/<username>')
+def hello_user(username):
+    return 'Hello %s!\n' % username
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
